eec2464d9d373a9d6f4b9086b6500734393b385d
Kyle Thompson <me@kkt.dev>
2022-11-17 16:05:14 -0500

    Parse Ruby RSpec results into RWX test results
    Original PR: 50
    Original Commit: 947fa7cb6e62f4d153015835aa79268fe9244f15

---
+++ internal/parsing/ruby_rspec_parser.go
@@ -0,0 +1,144 @@
+package parsing
+
+import (
+	"encoding/json"
+	"io"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/rwx-research/captain-cli/internal/errors"
+	v1 "github.com/rwx-research/captain-cli/internal/testingschema/v1"
+)
+
+type RubyRSpecParser struct{}
+
+type RubyRSpecException struct {
+	Class     string   `json:"class"`
+	Message   string   `json:"message"`
+	Backtrace []string `json:"backtrace"`
+}
+
+type RubyRSpecExample struct {
+	ID              *string             `json:"id"`
+	Description     string              `json:"description"`
+	FullDescription string              `json:"full_description"`
+	Status          string              `json:"status"`
+	FilePath        string              `json:"file_path"`
+	LineNumber      int                 `json:"line_number"`
+	RunTime         float64             `json:"run_time"`
+	PendingMessage  *string             `json:"pending_message"`
+	Exception       *RubyRSpecException `json:"exception"`
+}
+
+type RubyRSpecSummary struct {
+	Duration                     float64 `json:"duration"`
+	ExampleCount                 int     `json:"example_count"`
+	FailureCount                 int     `json:"failure_count"`
+	PendingCount                 int     `json:"pending_count"`
+	ErrorsOutsideOfExamplesCount int     `json:"errors_outside_of_examples_count"`
+}
+
+type RubyRSpecTestResults struct {
+	Version     *string            `json:"version"`
+	Messages    []string           `json:"messages"`
+	Examples    []RubyRSpecExample `json:"examples"`
+	Summary     *RubyRSpecSummary  `json:"summary"`
+	SummaryLine *string            `json:"summary_line"`
+}
+
+var fileRegexp = regexp.MustCompile(`\.rb(:.+|\[.+\])$`)
+
+func (p RubyRSpecParser) Parse(data io.Reader) (*ParseResult, error) {
+	var testResults RubyRSpecTestResults
+
+	if err := json.NewDecoder(data).Decode(&testResults); err != nil {
+		return nil, errors.NewInputError("Unable to parse test results as JSON: %s", err)
+	}
+	if testResults.Examples == nil {
+		return nil, errors.NewInputError("No examples were found in the JSON")
+	}
+	if testResults.Summary == nil {
+		return nil, errors.NewInputError("No summary was found in the JSON")
+	}
+	if len(testResults.Examples) > 0 && testResults.Examples[0].FullDescription == "" {
+		return nil, errors.NewInputError("The examples in the JSON do not appear to match RSpec JSON")
+	}
+
+	tests := make([]v1.Test, 0)
+	for _, example := range testResults.Examples {
+		id := example.ID
+		name := example.FullDescription
+
+		lineage := []string{
+			strings.TrimSpace(strings.TrimSuffix(example.FullDescription, example.Description)),
+			example.Description,
+		}
+
+		file := example.FilePath
+		if example.ID != nil {
+			file = fileRegexp.ReplaceAllString(*example.ID, ".rb")
+		}
+		location := v1.Location{File: file}
+
+		duration := time.Duration(example.RunTime * 1000000000)
+		meta := make(map[string]any)
+
+		if example.FilePath != "" {
+			meta["filePath"] = example.FilePath
+		}
+		if example.LineNumber != 0 {
+			meta["lineNumber"] = example.LineNumber
+		}
+
+		var status v1.TestStatus
+		switch example.Status {
+		case "failed":
+			status = v1.NewFailedTestStatus(
+				&example.Exception.Message,
+				&example.Exception.Class,
+				example.Exception.Backtrace,
+			)
+		case "passed":
+			status = v1.NewSuccessfulTestStatus()
+		case "pending":
+			status = v1.NewPendedTestStatus(example.PendingMessage)
+		default:
+			return nil, errors.NewInputError("Unexpected status %q for example %v", example.Status, example)
+		}
+
+		attempt := v1.TestAttempt{Duration: &duration, Meta: meta, Status: status}
+		tests = append(
+			tests,
+			v1.Test{
+				ID:       id,
+				Name:     name,
+				Lineage:  lineage,
+				Location: &location,
+				Attempt:  attempt,
+			},
+		)
+	}
+
+	otherErrors := make([]v1.OtherError, 0)
+	if testResults.Summary.ErrorsOutsideOfExamplesCount > 0 {
+		for i, message := range testResults.Messages {
+			if i >= testResults.Summary.ErrorsOutsideOfExamplesCount {
+				break
+			}
+
+			otherErrors = append(otherErrors, v1.OtherError{Message: message})
+		}
+	}
+
+	return &ParseResult{
+		Sentiment: PositiveParseResultSentiment,
+		TestResults: v1.TestResults{
+			Framework:   v1.NewRubyRSpecFramework(),
+			Summary:     v1.NewSummary(tests, otherErrors),
+			Tests:       tests,
+			OtherErrors: otherErrors,
+		},
+		Parser: p,
+	}, nil
+}
---
+++ internal/parsing/ruby_rspec_parser_test.go
@@ -0,0 +1,223 @@
+package parsing_test
+
+import (
+	"encoding/json"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/rwx-research/captain-cli/internal/parsing"
+	v1 "github.com/rwx-research/captain-cli/internal/testingschema/v1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("RubyRSpecParser", func() {
+	Describe("Parse", func() {
+		It("parses the sample file", func() {
+			fixture, err := os.Open("../../test/fixtures/rspec.json")
+			Expect(err).ToNot(HaveOccurred())
+
+			parseResult, err := parsing.RubyRSpecParser{}.Parse(fixture)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			Expect(parseResult.Parser).To(Equal(parsing.RubyRSpecParser{}))
+			Expect(parseResult.Sentiment).To(Equal(parsing.PositiveParseResultSentiment))
+			Expect(parseResult.TestResults.Framework.Language).To(Equal(v1.FrameworkLanguageRuby))
+			Expect(parseResult.TestResults.Framework.Kind).To(Equal(v1.FrameworkKindRSpec))
+			Expect(parseResult.TestResults.Summary.Tests).To(Equal(72))
+			Expect(parseResult.TestResults.Summary.Failed).To(Equal(36))
+			Expect(parseResult.TestResults.Summary.Pended).To(Equal(24))
+			Expect(parseResult.TestResults.Summary.Successful).To(Equal(12))
+			Expect(parseResult.TestResults.Summary.OtherErrors).To(Equal(0))
+		})
+
+		It("errors on malformed JSON", func() {
+			parseResult, err := parsing.RubyRSpecParser{}.Parse(strings.NewReader(`{"examples":[`))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Unable to parse test results as JSON"))
+			Expect(parseResult).To(BeNil())
+		})
+
+		It("errors JSON that doesn't look like RSpec", func() {
+			var parseResult *parsing.ParseResult
+			var err error
+
+			parseResult, err = parsing.RubyRSpecParser{}.Parse(strings.NewReader(`{}`))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("No examples were found in the JSON"))
+			Expect(parseResult).To(BeNil())
+
+			parseResult, err = parsing.RubyRSpecParser{}.Parse(strings.NewReader(`{"examples": []}`))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("No summary was found in the JSON"))
+			Expect(parseResult).To(BeNil())
+
+			parseResult, err = parsing.RubyRSpecParser{}.Parse(
+				strings.NewReader(`{"examples": [{}], "summary": {}}`),
+			)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("The examples in the JSON do not appear to match RSpec JSON"))
+			Expect(parseResult).To(BeNil())
+		})
+
+		It("parses examples that failed", func() {
+			id := "./spec/some/id_spec.rb:[1:2:3]"
+			rspecResults := parsing.RubyRSpecTestResults{
+				Examples: []parsing.RubyRSpecExample{
+					{
+						ID:              &id,
+						Description:     "the test description",
+						FullDescription: "Some::Class the test description",
+						Status:          "failed",
+						FilePath:        "./spec/some/other/file/path_spec.rb",
+						LineNumber:      12,
+						RunTime:         60.0,
+						PendingMessage:  nil,
+						Exception: &parsing.RubyRSpecException{
+							Class:     "ExceptionClass",
+							Message:   "ExceptionMessage",
+							Backtrace: []string{"1", "2", "3"},
+						},
+					},
+				},
+				Summary: &parsing.RubyRSpecSummary{},
+			}
+			data, err := json.Marshal(rspecResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.RubyRSpecParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			duration := time.Duration(60000000000)
+			Expect(parseResult.TestResults.Tests[0]).To(Equal(
+				v1.Test{
+					ID:       &id,
+					Name:     "Some::Class the test description",
+					Lineage:  []string{"Some::Class", "the test description"},
+					Location: &v1.Location{File: "./spec/some/id_spec.rb"},
+					Attempt: v1.TestAttempt{
+						Duration: &duration,
+						Meta:     map[string]any{"filePath": "./spec/some/other/file/path_spec.rb", "lineNumber": 12},
+						Status: v1.NewFailedTestStatus(
+							&rspecResults.Examples[0].Exception.Message,
+							&rspecResults.Examples[0].Exception.Class,
+							[]string{"1", "2", "3"},
+						),
+					},
+				},
+			))
+		})
+
+		It("parses successful examples", func() {
+			rspecResults := parsing.RubyRSpecTestResults{
+				Examples: []parsing.RubyRSpecExample{
+					{
+						Description:     "the test description",
+						FullDescription: "Some::Class the test description",
+						Status:          "passed",
+						FilePath:        "./spec/some/other/file/path_spec.rb",
+						LineNumber:      12,
+						RunTime:         60.0,
+						PendingMessage:  nil,
+					},
+				},
+				Summary: &parsing.RubyRSpecSummary{},
+			}
+			data, err := json.Marshal(rspecResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.RubyRSpecParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			duration := time.Duration(60000000000)
+			Expect(parseResult.TestResults.Tests[0]).To(Equal(
+				v1.Test{
+					Name:     "Some::Class the test description",
+					Lineage:  []string{"Some::Class", "the test description"},
+					Location: &v1.Location{File: "./spec/some/other/file/path_spec.rb"},
+					Attempt: v1.TestAttempt{
+						Duration: &duration,
+						Meta:     map[string]any{"filePath": "./spec/some/other/file/path_spec.rb", "lineNumber": 12},
+						Status:   v1.NewSuccessfulTestStatus(),
+					},
+				},
+			))
+		})
+
+		It("parses pending examples", func() {
+			id := "./spec/some/id_spec.rb:[1:2:3]"
+			pendingMessage := "pending message"
+			rspecResults := parsing.RubyRSpecTestResults{
+				Examples: []parsing.RubyRSpecExample{
+					{
+						ID:              &id,
+						Description:     "the test description",
+						FullDescription: "Some::Class the test description",
+						Status:          "pending",
+						FilePath:        "./spec/some/other/file/path_spec.rb",
+						LineNumber:      12,
+						RunTime:         60.0,
+						PendingMessage:  &pendingMessage,
+					},
+				},
+				Summary: &parsing.RubyRSpecSummary{},
+			}
+			data, err := json.Marshal(rspecResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.RubyRSpecParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			duration := time.Duration(60000000000)
+			Expect(parseResult.TestResults.Tests[0]).To(Equal(
+				v1.Test{
+					ID:       &id,
+					Name:     "Some::Class the test description",
+					Lineage:  []string{"Some::Class", "the test description"},
+					Location: &v1.Location{File: "./spec/some/id_spec.rb"},
+					Attempt: v1.TestAttempt{
+						Duration: &duration,
+						Meta:     map[string]any{"filePath": "./spec/some/other/file/path_spec.rb", "lineNumber": 12},
+						Status:   v1.NewPendedTestStatus(&pendingMessage),
+					},
+				},
+			))
+		})
+
+		It("fails to parse other statuses", func() {
+			id := "./spec/some/id_spec.rb:[1:2:3]"
+			rspecResults := parsing.RubyRSpecTestResults{
+				Examples: []parsing.RubyRSpecExample{
+					{
+						ID:              &id,
+						Description:     "the test description",
+						FullDescription: "Some::Class the test description",
+						Status:          "wat",
+						FilePath:        "./spec/some/other/file/path_spec.rb",
+						LineNumber:      12,
+						RunTime:         60.0,
+						PendingMessage:  nil,
+					},
+				},
+				Summary: &parsing.RubyRSpecSummary{},
+			}
+			data, err := json.Marshal(rspecResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.RubyRSpecParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Unexpected status \"wat\""))
+			Expect(parseResult).To(BeNil())
+		})
+	})
+})
