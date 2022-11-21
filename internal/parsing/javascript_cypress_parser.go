b74af5470bc95cb03543cf95ce82542faa26faa4
Kyle Thompson <me@kkt.dev>
2022-11-21 17:40:03 -0500

    Transform Cypress XML to RWX test results
    Original PR: 59
    Original Commit: 852d24879e0c211daf8468788fb1cc6728202a63

--- internal/parsing/javascript_cypress_parser.go
+++ internal/parsing/javascript_cypress_parser.go
@@ -2,7 +2,12 @@ package parsing
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io"
+	"math"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/rwx-research/captain-cli/internal/errors"
 	v1 "github.com/rwx-research/captain-cli/internal/testingschema/v1"
@@ -12,42 +17,42 @@ import (
 type JavaScriptCypressParser struct{}
 
 type JavaScriptCypressFailure struct {
-	Type     string `xml:"type,attr"`
-	Message  string `xml:"message,attr"`
-	Contents string `xml:",cdata"`
-}
-
-type JavaScriptCypressError struct {
-	Type     string `xml:"type,attr"`
-	Message  string `xml:"message,attr"`
-	Contents string `xml:",cdata"`
+	Type     *string `xml:"type,attr"`
+	Message  *string `xml:"message,attr"`
+	Contents string  `xml:",cdata"`
 }
 
 type JavaScriptCypressSkipped struct{}
 
 type JavaScriptCypressTestCase struct {
 	ClassName string                    `xml:"classname,attr"`
-	Name      string                    `xml:"name,attr"`
-	Time      float64                   `xml:"time,attr"`
+	Error     *JavaScriptCypressFailure `xml:"error"`
 	Failure   *JavaScriptCypressFailure `xml:"failure"`
-	Error     *JavaScriptCypressError   `xml:"error"`
+	Name      string                    `xml:"name,attr"`
 	Skipped   *JavaScriptCypressSkipped `xml:"skipped"`
 	SystemErr *string                   `xml:"system-err"`
 	SystemOut *string                   `xml:"system-out"`
+	Time      float64                   `xml:"time,attr"`
 	XMLName   xml.Name                  `xml:"testcase"`
 }
 
+type JavaScriptCypressProperty struct {
+	Name  string `xml:"name,attr"`
+	Value string `xml:"value,attr"`
+}
+
 type JavaScriptCypressTestSuite struct {
-	Errors    int                         `xml:"errors,attr"`
-	Failures  int                         `xml:"failures,attr"`
-	File      *string                     `xml:"file,attr"`
-	Name      string                      `xml:"name,attr"`
-	Skipped   int                         `xml:"skipped,attr"`
-	TestCases []JavaScriptCypressTestCase `xml:"testcase"`
-	Tests     int                         `xml:"tests,attr"`
-	Time      float64                     `xml:"time,attr"`
-	Timestamp string                      `xml:"timestamp,attr"`
-	XMLName   xml.Name                    `xml:"testsuite"`
+	Errors     int                         `xml:"errors,attr"`
+	Failures   int                         `xml:"failures,attr"`
+	File       *string                     `xml:"file,attr"`
+	Name       string                      `xml:"name,attr"`
+	Skipped    int                         `xml:"skipped,attr"`
+	TestCases  []JavaScriptCypressTestCase `xml:"testcase"`
+	Properties []JavaScriptCypressProperty `xml:"properties>property"`
+	Tests      int                         `xml:"tests,attr"`
+	Time       float64                     `xml:"time,attr"`
+	Timestamp  string                      `xml:"timestamp,attr"`
+	XMLName    xml.Name                    `xml:"testsuite"`
 }
 
 type JavaScriptCypressTestResults struct {
@@ -58,6 +63,8 @@ type JavaScriptCypressTestResults struct {
 	TestSuites []JavaScriptCypressTestSuite `xml:"testsuite"`
 	XMLName    xml.Name                     `xml:"testsuites"`
 }
+
+var javaScriptCypressNewlineRegexp = regexp.MustCompile(`\r?\n`)
 
 func (p JavaScriptCypressParser) Parse(data io.Reader) (*ParseResult, error) {
 	var testResults JavaScriptCypressTestResults
@@ -73,15 +80,103 @@ func (p JavaScriptCypressParser) Parse(data io.Reader) (*ParseResult, error) {
 	}
 
 	tests := make([]v1.Test, 0)
-	otherErrors := make([]v1.OtherError, 0)
+	var currentFile *string
+	for _, testSuite := range testResults.TestSuites {
+		if testSuite.File != nil {
+			currentFile = testSuite.File
+
+			if !(strings.Contains(*currentFile, ".cy.") || strings.Contains(*currentFile, "cypress/")) {
+				return nil, errors.NewInputError("The file does not look like a Cypress file: %q", *currentFile)
+			}
+		}
+
+		var properties map[string]any
+		if len(testSuite.Properties) > 0 {
+			properties = make(map[string]any)
+		}
+		for _, property := range testSuite.Properties {
+			properties[property.Name] = property.Value
+		}
+
+		for _, testCase := range testSuite.TestCases {
+			// The mocha junit reporter library allows switching these
+			// We want the one that has the entire description (contains the short description)
+			// e.g. classname="Some Tests with some context it passes" name="it passes"
+			// We'd want the classname in the above case. Classname contains name, but name doesn't contain classname
+			var name string
+			switch {
+			case strings.Contains(testCase.Name, testCase.ClassName) && strings.Contains(testCase.ClassName, testCase.Name):
+				name = testCase.Name
+			case !strings.Contains(testCase.Name, testCase.ClassName) && strings.Contains(testCase.ClassName, testCase.Name):
+				name = testCase.ClassName
+			case strings.Contains(testCase.Name, testCase.ClassName) && !strings.Contains(testCase.ClassName, testCase.Name):
+				name = testCase.Name
+			case !strings.Contains(testCase.Name, testCase.ClassName) && !strings.Contains(testCase.ClassName, testCase.Name):
+				name = fmt.Sprintf("%s %s", testCase.ClassName, testCase.Name)
+			default:
+				return nil, errors.NewInternalError("Unreachable: reached default case of exhaustive switch statement")
+			}
+
+			duration := time.Duration(math.Round(testCase.Time * float64(time.Second)))
+
+			var status v1.TestStatus
+			switch {
+			case testCase.Failure != nil:
+				failureMessage := testCase.Failure.Message
+				failureException := testCase.Failure.Type
+
+				lines := javaScriptCypressNewlineRegexp.Split(testCase.Failure.Contents, -1)
+				var backtrace []string
+				for _, line := range lines {
+					if failureException != nil &&
+						failureMessage != nil &&
+						line == fmt.Sprintf("%s: %s", *failureException, *failureMessage) {
+						continue
+					}
+
+					backtrace = append(backtrace, strings.TrimSpace(line))
+				}
+
+				status = v1.NewFailedTestStatus(failureMessage, failureException, backtrace)
+			case testCase.Skipped != nil:
+				status = v1.NewSkippedTestStatus(nil)
+			case testCase.Error != nil:
+				return nil, errors.NewInputError(
+					"Unexpected <error> element in %q, mocha-junit-reporter does not emit these.",
+					name,
+				)
+			default:
+				status = v1.NewSuccessfulTestStatus()
+			}
+
+			var location *v1.Location
+			if currentFile != nil {
+				location = &v1.Location{File: *currentFile}
+			}
+
+			tests = append(
+				tests,
+				v1.Test{
+					Name:     name,
+					Location: location,
+					Attempt: v1.TestAttempt{
+						Duration: &duration,
+						Meta:     properties,
+						Status:   status,
+						Stderr:   testCase.SystemErr,
+						Stdout:   testCase.SystemOut,
+					},
+				},
+			)
+		}
+	}
 
 	return &ParseResult{
 		Sentiment: PositiveParseResultSentiment,
 		TestResults: v1.TestResults{
-			Framework:   v1.NewJavaScriptCypressFramework(),
-			Summary:     v1.NewSummary(tests, otherErrors),
-			Tests:       tests,
-			OtherErrors: otherErrors,
+			Framework: v1.NewJavaScriptCypressFramework(),
+			Summary:   v1.NewSummary(tests, nil),
+			Tests:     tests,
 		},
 		Parser: p,
 	}, nil
--- internal/parsing/javascript_cypress_parser_test.go
+++ internal/parsing/javascript_cypress_parser_test.go
@@ -3,6 +3,7 @@ package parsing_test
 import (
 	"os"
 	"strings"
+	"time"
 
 	"github.com/rwx-research/captain-cli/internal/parsing"
 	v1 "github.com/rwx-research/captain-cli/internal/testingschema/v1"
@@ -25,7 +26,10 @@ var _ = Describe("JavaScriptCypressParser", func() {
 			Expect(parseResult.Sentiment).To(Equal(parsing.PositiveParseResultSentiment))
 			Expect(parseResult.TestResults.Framework.Language).To(Equal(v1.FrameworkLanguageJavaScript))
 			Expect(parseResult.TestResults.Framework.Kind).To(Equal(v1.FrameworkKindCypress))
-			Expect(parseResult.TestResults.Summary.Tests).To(Equal(0))
+			Expect(parseResult.TestResults.Summary.Tests).To(Equal(19))
+			Expect(parseResult.TestResults.Summary.Successful).To(Equal(14))
+			Expect(parseResult.TestResults.Summary.Failed).To(Equal(3))
+			Expect(parseResult.TestResults.Summary.Skipped).To(Equal(2))
 			Expect(parseResult.TestResults.Summary.OtherErrors).To(Equal(0))
 		})
 
@@ -58,6 +62,260 @@ var _ = Describe("JavaScriptCypressParser", func() {
 				ContainSubstring("The test suites in the XML do not appear to match Cypress XML"),
 			)
 			Expect(parseResult).To(BeNil())
+		})
+
+		It("parses files properly", func() {
+			fixture, err := os.Open("../../test/fixtures/cypress.xml")
+			Expect(err).ToNot(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(fixture)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			nameOne := "example to-do app one displays two todo items by default"
+			nameTwo := "example to-do app two can add new todo items"
+			for _, test := range parseResult.TestResults.Tests {
+				switch test.Name {
+				case nameOne:
+					Expect(test.Location.File).To(Equal("cypress/e2e/todo-one.cy.js"))
+				case nameTwo:
+					Expect(test.Location.File).To(Equal("cypress/e2e/todo-two.cy.js"))
+				default:
+					continue
+				}
+				return
+			}
+
+			Expect(true).To(Equal(false), "Unreachable")
+		})
+
+		It("parses successful tests", func() {
+			fixture, err := os.Open("../../test/fixtures/cypress.xml")
+			Expect(err).ToNot(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(fixture)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			name := "displays two todo items by default"
+			for _, test := range parseResult.TestResults.Tests {
+				if test.Name != name {
+					continue
+				}
+
+				Expect(test.Attempt.Status).To(Equal(v1.NewSuccessfulTestStatus()))
+				Expect(*test.Attempt.Duration).To(Equal(time.Duration(490000000)))
+
+				return
+			}
+
+			Expect(true).To(Equal(false), "Unreachable")
+		})
+
+		It("parses tests which failed via an assertion", func() {
+			fixture, err := os.Open("../../test/fixtures/cypress.xml")
+			Expect(err).ToNot(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(fixture)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			name := "example to-do app one displays two todo items by default"
+			for _, test := range parseResult.TestResults.Tests {
+				if test.Name != name {
+					continue
+				}
+
+				message := "Timed out retrying after 4000ms: expected '<li>' to have text 'Walk the cat', but" +
+					" the text was 'Walk the dog'"
+				class := "AssertionError"
+				backtrace := []string{
+					"at Context.eval (webpack:///./cypress/e2e/todo-one.cy.js:59:35)",
+					"",
+					"+ expected - actual",
+					"",
+					"-'Walk the dog'",
+					"+'Walk the cat'",
+					"",
+				}
+				Expect(test.Attempt.Status).To(Equal(v1.NewFailedTestStatus(&message, &class, backtrace)))
+
+				return
+			}
+
+			Expect(true).To(Equal(false), "Unreachable")
+		})
+
+		It("parses tests which failed via a custom error", func() {
+			fixture, err := os.Open("../../test/fixtures/cypress.xml")
+			Expect(err).ToNot(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(fixture)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			name := "example to-do app one can add new todo items"
+			for _, test := range parseResult.TestResults.Tests {
+				if test.Name != name {
+					continue
+				}
+
+				message := "uh oh something broke"
+				class := "Error"
+				backtrace := []string{"at Context.eval (webpack:///./cypress/e2e/todo-one.cy.js:73:10)"}
+				Expect(test.Attempt.Status).To(Equal(v1.NewFailedTestStatus(&message, &class, backtrace)))
+
+				return
+			}
+
+			Expect(true).To(Equal(false), "Unreachable")
+		})
+
+		It("parses tests which failed via a custom error", func() {
+			fixture, err := os.Open("../../test/fixtures/cypress.xml")
+			Expect(err).ToNot(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(fixture)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			name := "example to-do app one with a checked task can filter for uncompleted tasks"
+			for _, test := range parseResult.TestResults.Tests {
+				if test.Name != name {
+					continue
+				}
+
+				message := "not a custom error"
+				class := "Error"
+				backtrace := []string{
+					"at $Cy.fail (https://example.cypress.io/__cypress/runner/cypress_runner.js:157093:13)",
+					"at runnable.fn (https://example.cypress.io/__cypress/runner/cypress_runner.js:157707:19)",
+					"at callFn (https://example.cypress.io/__cypress/runner/cypress_runner.js:107910:21)",
+					"at ../driver/node_modules/mocha/lib/runnable.js.Runnable.run" +
+						" (https://example.cypress.io/__cypress/runner/cypress_runner.js:107897:7)",
+					"at <unknown> (https://example.cypress.io/__cypress/runner/cypress_runner.js:164958:30)",
+					"at PassThroughHandlerContext.finallyHandler" +
+						" (https://example.cypress.io/__cypress/runner/cypress_runner.js:7872:23)",
+					"at PassThroughHandlerContext.tryCatcher" +
+						" (https://example.cypress.io/__cypress/runner/cypress_runner.js:11318:23)",
+					"at Promise._settlePromiseFromHandler (https://example.cypress.io/__cypress/runner/cypress_runner.js:9253:31)",
+					"at Promise._settlePromise (https://example.cypress.io/__cypress/runner/cypress_runner.js:9310:18)",
+					"at Promise._settlePromise0 (https://example.cypress.io/__cypress/runner/cypress_runner.js:9355:10)",
+					"at Promise._settlePromises (https://example.cypress.io/__cypress/runner/cypress_runner.js:9435:18)",
+					"at _drainQueueStep (https://example.cypress.io/__cypress/runner/cypress_runner.js:6025:12)",
+					"at _drainQueue (https://example.cypress.io/__cypress/runner/cypress_runner.js:6018:9)",
+					"at ../../node_modules/bluebird/js/release/async.js.Async._drainQueues" +
+						" (https://example.cypress.io/__cypress/runner/cypress_runner.js:6034:5)",
+					"at Async.drainQueues (https://example.cypress.io/__cypress/runner/cypress_runner.js:5904:14)",
+				}
+				Expect(test.Attempt.Status).To(Equal(v1.NewFailedTestStatus(&message, &class, backtrace)))
+
+				return
+			}
+
+			Expect(true).To(Equal(false), "Unreachable")
+		})
+
+		It("errors when presented with an <error> element", func() {
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(strings.NewReader(
+				`
+					<testsuites tests="1">
+						<testsuite file="cypress/e2e/some.test.cy.js">
+							<testcase name="some test name">
+								<error />
+							</testcase>
+						</testsuite>
+					</testsuites>
+				`,
+			))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring(
+				`Unexpected <error> element in "some test name", mocha-junit-reporter does not emit these.`,
+			))
+			Expect(parseResult).To(BeNil())
+		})
+
+		It("errors when the file name doesn't look like Cypress", func() {
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(strings.NewReader(
+				`
+					<testsuites tests="1">
+						<testsuite file="e2e/some.test.js">
+							<testcase name="some test name">
+								<error />
+							</testcase>
+						</testsuite>
+					</testsuites>
+				`,
+			))
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring(
+				`The file does not look like a Cypress file: "e2e/some.test.js"`,
+			))
+			Expect(parseResult).To(BeNil())
+		})
+
+		It("calculates the correct name when the classname contains the name", func() {
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(strings.NewReader(
+				`
+					<testsuites tests="1">
+						<testsuite file="cypress/e2e/some.test.cy.js">
+							<testcase name="some test name" classname="prefix some test name">
+							</testcase>
+						</testsuite>
+					</testsuites>
+				`,
+			))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Name).To(Equal("prefix some test name"))
+		})
+
+		It("calculates the correct name when the name contains the classname", func() {
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(strings.NewReader(
+				`
+					<testsuites tests="1">
+						<testsuite file="cypress/e2e/some.test.cy.js">
+							<testcase name="prefix some test name" classname="some test name">
+							</testcase>
+						</testsuite>
+					</testsuites>
+				`,
+			))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Name).To(Equal("prefix some test name"))
+		})
+
+		It("calculates the correct name when the name is the same as classname", func() {
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(strings.NewReader(
+				`
+					<testsuites tests="1">
+						<testsuite file="cypress/e2e/some.test.cy.js">
+							<testcase name="prefix some test name" classname="prefix some test name">
+							</testcase>
+						</testsuite>
+					</testsuites>
+				`,
+			))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Name).To(Equal("prefix some test name"))
+		})
+
+		It("calculates the correct name when the name is entirely different from the classname", func() {
+			parseResult, err := parsing.JavaScriptCypressParser{}.Parse(strings.NewReader(
+				`
+					<testsuites tests="1">
+						<testsuite file="cypress/e2e/some.test.cy.js">
+							<testcase name="some test name" classname="prefix">
+							</testcase>
+						</testsuite>
+					</testsuites>
+				`,
+			))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Name).To(Equal("prefix some test name"))
 		})
 	})
 })
