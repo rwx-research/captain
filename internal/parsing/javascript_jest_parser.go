c0f5a288cba153f9c7735d4684c5e40da0a2bb6f
Kyle Thompson <me@kkt.dev>
2022-11-21 13:23:42 -0500

    Jest Results -> RWX Test Results
    Original PR: 55
    Original Commit: 112c7b537f92784f6f5448e5b39e34e8ac1d6229

--- internal/parsing/javascript_jest_parser.go
+++ internal/parsing/javascript_jest_parser.go
@@ -2,7 +2,11 @@ package parsing
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/rwx-research/captain-cli/internal/errors"
 	v1 "github.com/rwx-research/captain-cli/internal/testingschema/v1"
@@ -99,9 +103,13 @@ type JavaScriptJestTestResults struct {
 
 	// Not in the spec
 	NumTodoTests *int                             `json:"numTodoTests"`
-	OpenHandles  []int                            `json:"openHandles"`
+	OpenHandles  []any                            `json:"openHandles"`
 	RunExecError *JavaScriptJestSerializableError `json:"runExecError"`
 }
+
+var newlineRegexp = regexp.MustCompile(`\r?\n`)
+
+var backtraceSeparatorRegexp = regexp.MustCompile(`\r?\n\s{4}at`)
 
 func (p JavaScriptJestParser) Parse(data io.Reader) (*ParseResult, error) {
 	var testResults JavaScriptJestTestResults
@@ -124,6 +132,102 @@ func (p JavaScriptJestParser) Parse(data io.Reader) (*ParseResult, error) {
 
 	tests := make([]v1.Test, 0)
 	otherErrors := make([]v1.OtherError, 0)
+	for _, testResult := range testResults.TestResults {
+		sawFailedTest := false
+		file := testResult.Name
+
+		for _, assertionResult := range testResult.AssertionResults {
+			lineage := assertionResult.AncestorTitles
+			lineage = append(lineage, assertionResult.Title)
+			name := strings.Join(lineage, " > ")
+
+			var line *int
+			var column *int
+			if assertionResult.Location != nil {
+				line = &assertionResult.Location.Line
+				column = &assertionResult.Location.Column
+			}
+			location := v1.Location{File: file, Line: line, Column: column}
+
+			var duration *time.Duration
+			if assertionResult.Duration != nil {
+				transformedDuration := time.Duration(*assertionResult.Duration) * time.Millisecond
+				duration = &transformedDuration
+			}
+
+			var status v1.TestStatus
+			switch assertionResult.Status {
+			case "passed":
+				status = v1.NewSuccessfulTestStatus()
+			case "failed":
+				message, backtrace := p.extractFailureMetadata(assertionResult.FailureMessages)
+				status = v1.NewFailedTestStatus(message, nil, backtrace)
+				sawFailedTest = true
+			case "skipped":
+				status = v1.NewSkippedTestStatus(nil)
+			case "pending":
+				status = v1.NewPendedTestStatus(nil)
+			case "todo":
+				status = v1.NewTodoTestStatus(nil)
+			default:
+				return nil, errors.NewInputError(
+					"Unexpected status %q for assertion result %v",
+					assertionResult.Status,
+					assertionResult,
+				)
+			}
+
+			attempt := v1.TestAttempt{Duration: duration, Status: status}
+
+			var pastAttempts []v1.TestAttempt
+			switch {
+			case assertionResult.RetryReasons != nil:
+				for _, retryReason := range assertionResult.RetryReasons {
+					message, backtrace := p.extractFailureMetadata([]string{retryReason})
+					status := v1.NewFailedTestStatus(message, nil, backtrace)
+					pastAttempts = append(pastAttempts, v1.TestAttempt{Status: status})
+				}
+			case assertionResult.Invocations != nil && *assertionResult.Invocations > 1:
+				for i := 0; i < *assertionResult.Invocations-1; i++ {
+					status := v1.NewFailedTestStatus(nil, nil, nil)
+					pastAttempts = append(pastAttempts, v1.TestAttempt{Status: status})
+				}
+			default:
+				pastAttempts = nil
+			}
+
+			tests = append(
+				tests,
+				v1.Test{
+					Name:         name,
+					Lineage:      lineage,
+					Location:     &location,
+					Attempt:      attempt,
+					PastAttempts: pastAttempts,
+				},
+			)
+		}
+
+		if !sawFailedTest && testResult.Status == "failed" {
+			otherErrors = append(otherErrors, v1.OtherError{Message: testResult.Message})
+		}
+	}
+
+	if testResults.RunExecError != nil {
+		var backtrace []string
+		if testResults.RunExecError.Stack != nil {
+			backtrace = newlineRegexp.Split(*testResults.RunExecError.Stack, -1)
+		}
+		otherErrors = append(
+			otherErrors,
+			v1.OtherError{Message: testResults.RunExecError.Message, Backtrace: backtrace},
+		)
+	}
+
+	for range testResults.OpenHandles {
+		otherErrors = append(otherErrors, v1.OtherError{Message: "An open handle was detected"})
+	}
+
 	return &ParseResult{
 		Sentiment: PositiveParseResultSentiment,
 		TestResults: v1.TestResults{
@@ -134,4 +238,21 @@ func (p JavaScriptJestParser) Parse(data io.Reader) (*ParseResult, error) {
 		},
 		Parser: p,
 	}, nil
+}
+
+func (p JavaScriptJestParser) extractFailureMetadata(failureMessages []string) (*string, []string) {
+	var message *string
+	var backtrace []string
+
+	if failureMessages != nil && failureMessages[0] != "" {
+		parts := backtraceSeparatorRegexp.Split(failureMessages[0], -1)
+		first, rest := parts[0], parts[1:]
+		message = &first
+
+		for _, part := range rest {
+			backtrace = append(backtrace, fmt.Sprintf("at%s", part))
+		}
+	}
+
+	return message, backtrace
 }
--- internal/parsing/javascript_jest_parser_test.go
+++ internal/parsing/javascript_jest_parser_test.go
@@ -1,8 +1,10 @@
 package parsing_test
 
 import (
+	"encoding/json"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/rwx-research/captain-cli/internal/parsing"
 	v1 "github.com/rwx-research/captain-cli/internal/testingschema/v1"
@@ -25,9 +27,11 @@ var _ = Describe("JavaScriptJestParser", func() {
 			Expect(parseResult.Sentiment).To(Equal(parsing.PositiveParseResultSentiment))
 			Expect(parseResult.TestResults.Framework.Language).To(Equal(v1.FrameworkLanguageJavaScript))
 			Expect(parseResult.TestResults.Framework.Kind).To(Equal(v1.FrameworkKindJest))
-			Expect(parseResult.TestResults.Summary.Tests).To(Equal(0))
-			Expect(parseResult.TestResults.Summary.Failed).To(Equal(0))
-			Expect(parseResult.TestResults.Summary.Successful).To(Equal(0))
+			Expect(parseResult.TestResults.Summary.Tests).To(Equal(18))
+			Expect(parseResult.TestResults.Summary.Successful).To(Equal(6))
+			Expect(parseResult.TestResults.Summary.Failed).To(Equal(6))
+			Expect(parseResult.TestResults.Summary.Pended).To(Equal(3))
+			Expect(parseResult.TestResults.Summary.Todo).To(Equal(3))
 			Expect(parseResult.TestResults.Summary.OtherErrors).To(Equal(0))
 		})
 
@@ -69,6 +73,439 @@ var _ = Describe("JavaScriptJestParser", func() {
 				ContainSubstring("The test results in the JSON do not appear to match Jest JSON"),
 			)
 			Expect(parseResult).To(BeNil())
+		})
+
+		It("parses a minimally failing test", func() {
+			zero := 0
+			durationInMilliseconds := 653
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Duration: &durationInMilliseconds,
+								Status:   "failed",
+								Title:    "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			expectedDuration := time.Duration(653000000)
+			Expect(parseResult.TestResults.Tests[0]).To(Equal(
+				v1.Test{
+					Name:     "title",
+					Lineage:  []string{"title"},
+					Location: &v1.Location{File: "/some/path/to/name/of/file.js"},
+					Attempt: v1.TestAttempt{
+						Duration: &expectedDuration,
+						Status:   v1.NewFailedTestStatus(nil, nil, nil),
+					},
+				},
+			))
+		})
+
+		It("parses a maximally failing test", func() {
+			zero := 0
+			durationInMilliseconds := 653
+			invocations := 3
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								AncestorTitles:  []string{"part 1", "part 2"},
+								Duration:        &durationInMilliseconds,
+								FailureMessages: []string{"final\n\nmessage\n    at stacktrace"},
+								Status:          "failed",
+								Title:           "title",
+								Location:        &parsing.JavaScriptJestCallsite{Line: 10, Column: 12},
+								RetryReasons: []string{
+									"first\n\nmessage\n    at stacktrace\n    at other trace",
+									"second\n\nmessage",
+								},
+								Invocations: &invocations,
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			expectedDuration := time.Duration(653000000)
+			line := 10
+			column := 12
+			firstFailureMessage := "first\n\nmessage"
+			firstBacktrace := []string{"at stacktrace", "at other trace"}
+			secondFailureMessage := "second\n\nmessage"
+			finalFailureMessage := "final\n\nmessage"
+			finalBacktrace := []string{"at stacktrace"}
+			Expect(parseResult.TestResults.Tests[0]).To(Equal(
+				v1.Test{
+					Name:     "part 1 > part 2 > title",
+					Lineage:  []string{"part 1", "part 2", "title"},
+					Location: &v1.Location{File: "/some/path/to/name/of/file.js", Line: &line, Column: &column},
+					Attempt: v1.TestAttempt{
+						Duration: &expectedDuration,
+						Status:   v1.NewFailedTestStatus(&finalFailureMessage, nil, finalBacktrace),
+					},
+					PastAttempts: []v1.TestAttempt{
+						{Status: v1.NewFailedTestStatus(&firstFailureMessage, nil, firstBacktrace)},
+						{Status: v1.NewFailedTestStatus(&secondFailureMessage, nil, nil)},
+					},
+				},
+			))
+		})
+
+		It("does not require retry reasons to capture retries (it can use invocations)", func() {
+			zero := 0
+			durationInMilliseconds := 653
+			invocations := 3
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								AncestorTitles:  []string{"part 1", "part 2"},
+								Duration:        &durationInMilliseconds,
+								FailureMessages: []string{"final\n\nmessage\n    at stacktrace"},
+								Status:          "failed",
+								Title:           "title",
+								Location:        &parsing.JavaScriptJestCallsite{Line: 10, Column: 12},
+								Invocations:     &invocations,
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+
+			expectedDuration := time.Duration(653000000)
+			line := 10
+			column := 12
+			finalFailureMessage := "final\n\nmessage"
+			finalBacktrace := []string{"at stacktrace"}
+			Expect(parseResult.TestResults.Tests[0]).To(Equal(
+				v1.Test{
+					Name:     "part 1 > part 2 > title",
+					Lineage:  []string{"part 1", "part 2", "title"},
+					Location: &v1.Location{File: "/some/path/to/name/of/file.js", Line: &line, Column: &column},
+					Attempt: v1.TestAttempt{
+						Duration: &expectedDuration,
+						Status:   v1.NewFailedTestStatus(&finalFailureMessage, nil, finalBacktrace),
+					},
+					PastAttempts: []v1.TestAttempt{
+						{Status: v1.NewFailedTestStatus(nil, nil, nil)},
+						{Status: v1.NewFailedTestStatus(nil, nil, nil)},
+					},
+				},
+			))
+		})
+
+		It("parses passed statuses", func() {
+			zero := 0
+			durationInMilliseconds := 653
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Duration: &durationInMilliseconds,
+								Status:   "passed",
+								Title:    "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Attempt.Status).To(Equal(v1.NewSuccessfulTestStatus()))
+		})
+
+		It("parses todo statuses", func() {
+			zero := 0
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "todo",
+								Title:  "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Attempt.Duration).To(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Attempt.Status).To(Equal(v1.NewTodoTestStatus(nil)))
+		})
+
+		It("parses skipped statuses", func() {
+			zero := 0
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "skipped",
+								Title:  "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Attempt.Status).To(Equal(v1.NewSkippedTestStatus(nil)))
+		})
+
+		It("parses pending statuses", func() {
+			zero := 0
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "pending",
+								Title:  "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.Tests[0].Attempt.Status).To(Equal(v1.NewPendedTestStatus(nil)))
+		})
+
+		It("errors on other statuses", func() {
+			zero := 0
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "wat",
+								Title:  "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("Unexpected status \"wat\""))
+			Expect(parseResult).To(BeNil())
+		})
+
+		It("parses an other error when a whole test result fails", func() {
+			zero := 0
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:    "/some/path/to/name/of/file.js",
+						Status:  "failed",
+						Message: "the reason it failed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "passed",
+								Title:  "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.OtherErrors[0]).To(Equal(v1.OtherError{Message: "the reason it failed"}))
+			Expect(parseResult.TestResults.Tests[0]).NotTo(BeNil())
+		})
+
+		It("does not parse an other error when a whole test result fails due to a failing test", func() {
+			zero := 0
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:    "/some/path/to/name/of/file.js",
+						Status:  "failed",
+						Message: "the reason it failed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "failed",
+								Title:  "title",
+							},
+						},
+					},
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.OtherErrors).To(HaveLen(0))
+			Expect(parseResult.TestResults.Tests[0]).NotTo(BeNil())
+		})
+
+		It("parses an other error when there is a run exec error", func() {
+			zero := 0
+			stack := "the stacktrace\nwith multiple lines"
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "passed",
+								Title:  "title",
+							},
+						},
+					},
+				},
+				RunExecError: &parsing.JavaScriptJestSerializableError{
+					Message: "the reason it failed",
+					Stack:   &stack,
+				},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.OtherErrors[0]).To(Equal(
+				v1.OtherError{
+					Message:   "the reason it failed",
+					Backtrace: []string{"the stacktrace", "with multiple lines"},
+				},
+			))
+			Expect(parseResult.TestResults.Tests[0]).NotTo(BeNil())
+		})
+
+		It("parses an other error when there are open handles", func() {
+			zero := 0
+			jestResults := parsing.JavaScriptJestTestResults{
+				NumRuntimeErrorTestSuites: &zero,
+				Snapshot:                  &parsing.JavaScriptJestSnapshot{},
+				TestResults: []parsing.JavaScriptJestTestResult{
+					{
+						Name:   "/some/path/to/name/of/file.js",
+						Status: "passed",
+						AssertionResults: []parsing.JavaScriptJestAssertionResult{
+							{
+								Status: "passed",
+								Title:  "title",
+							},
+						},
+					},
+				},
+				OpenHandles: []any{struct{}{}},
+			}
+			data, err := json.Marshal(jestResults)
+			Expect(err).NotTo(HaveOccurred())
+
+			parseResult, err := parsing.JavaScriptJestParser{}.Parse(strings.NewReader(string(data)))
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(parseResult).NotTo(BeNil())
+			Expect(parseResult.TestResults.OtherErrors[0]).To(Equal(
+				v1.OtherError{Message: "An open handle was detected"},
+			))
+			Expect(parseResult.TestResults.Tests[0]).NotTo(BeNil())
 		})
 	})
 })
