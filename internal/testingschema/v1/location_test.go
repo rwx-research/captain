1b6d05eb4eeb0842706ab643df64d353e2278f14
Kyle Thompson <me@kkt.dev>
2023-04-06 14:30:19 -0400

    Test Location.String()

---
+++ internal/testingschema/v1/location_test.go
@@ -0,0 +1,21 @@
+package v1_test
+
+import (
+	v1 "github.com/rwx-research/captain-cli/internal/testingschema/v1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Location", func() {
+	Describe("String", func() {
+		It("formats when there is a line number", func() {
+			line := 15
+			Expect(v1.Location{File: "some/file.rb", Line: &line}.String()).To(Equal("some/file.rb:15"))
+		})
+
+		It("formats when there is not a line number", func() {
+			Expect(v1.Location{File: "some/file.rb"}.String()).To(Equal("some/file.rb"))
+		})
+	})
+})
